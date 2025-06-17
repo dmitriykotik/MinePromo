13e3781584cf234a059c109cd9b04484266e1476
dmitriykotik <dmitry090209@gmail.com>
2025-06-17 17:39:42 +0300

    Create

---
+++ src/main/java/org/dmitriykotik/minePromo/MinePromo.java
@@ -0,0 +1,17 @@
+package org.dmitriykotik.minePromo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinePromo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
